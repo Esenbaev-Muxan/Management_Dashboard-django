61b216ec0c132f9b8da980f6eda7dc380bb5d7ff
Student astrum <student@astrum.domain>
2023-02-15 12:53:41 +0500

    managemant system

---
+++ dashboard/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'dashboard/index.html')
+
+def staff(request):
+   return render(request, 'dashboard/staff.html')
