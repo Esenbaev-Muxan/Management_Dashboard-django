21af07fede98528ff68bf00fe9b6c76510044567
unknown <esenbaevmuxan@example.com>
2023-02-15 21:03:32 +0500

    create product model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY = (
+    ('Stationary', 'Stationary'),
+    ('Electronics', 'Electronics'),
+    ('Food', 'Food'),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+    
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'
