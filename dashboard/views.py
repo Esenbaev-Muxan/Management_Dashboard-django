4049e224e96a79065774f1552950f626224500b8
Student astrum <student@astrum.domain>
2023-02-15 18:03:22 +0500

    introduction tom database models

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,3 +8,9 @@ def index(request):
 
 def staff(request):
    return render(request, 'dashboard/staff.html')
+
+def product(request):
+   return render(request, 'dashboard/product.html')
+
+def order(request):
+   return render(request, 'dashboard/order.html')
