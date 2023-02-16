23ea097268fd492a72806ea6a0493b54bc20dc71
unknown <esenbaevmuxan@example.com>
2023-02-16 21:42:58 +0500

    Login and Logout authentication

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -132,4 +132,6 @@ STATIC_ROOT = (BASE_DIR/"asert/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = 'dashboard-index'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
