69cf5b68c5fe585515f06de5c88e76cadfee1665
Student astrum <student@astrum.domain>
2023-02-16 11:21:48 +0500

    Admin dashboard settings products

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Product 
+from django.contrib.auth.models import Group
 
 # Register your models here.
+admin.site.site_header = 'Dashboard'
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity')
+    list_filter = ['category']
+
+admin.site.register(Product, ProductAdmin)
+# admin.site.unregister(Group)
