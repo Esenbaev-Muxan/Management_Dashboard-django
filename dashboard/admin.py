baf6486063782a18ef95789de204c882b90a6439
Student astrum <student@astrum.domain>
2023-02-16 13:24:27 +0500

    Order models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product 
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 # Register your models here.
@@ -10,4 +10,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['category']
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
 # admin.site.unregister(Group)
