ee40ef6f51b0bc56462bfdf4465bc9892427721c
Student astrum <student@astrum.domain>
2023-02-17 09:01:51 +0500

    profile and image file

--- user/views.py
+++ user/views.py
@@ -18,3 +18,29 @@ def register(request):
     }
     return render(request, 'user/register.html', context)
 
+def profile(request):
+    return render(request, 'user/profile.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
