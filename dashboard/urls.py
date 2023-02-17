21ed79b2f54dc9227b9729570119ff6a36f59eed
Student astrum <student@astrum.domain>
2023-02-17 15:36:14 +0500

    product update delete

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('dashboard/', views.index, name='dashboard-index'),
     path('staff/', views.staff, name='dashboard-staff'),
     path('product/', views.product, name='dashboard-product'),
+    path('product/delete/<int:pk>/', views.product_delete, name='dashboard-product-delete'),
+    path('product/update/<int:pk>/', views.product_update, name='dashboard-product-update'),
     path('order/', views.order, name='dashboard-order'),
 ]
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 @login_required
@@ -13,8 +15,48 @@ def staff(request):
 
 @login_required
 def product(request):
-   return render(request, 'dashboard/product.html')
+   items = Product.objects.all() 
+   # items = Product.objects.raw('SELECT * FROM dashboard_product')
+
+   if request.method == 'POST':
+      form = ProductForm(request.POST)
+      if form.is_valid():
+         form.save()
+         return redirect('dashboard-product')
+   else:
+      form = ProductForm()
+
+   context = {
+      'items': items,
+      'form': form,
+   }
+   return render(request, 'dashboard/product.html', context)
+
+def product_delete(request, pk):
+   item = Product.objects.get(id=pk)
+   if request.method == 'POST':
+      item.delete()
+      return redirect('dashboard-product')
+   return render(request, 'dashboard/product_delete.html')
+
+
+def product_update(request, pk):
+   item = Product.objects.get(id=pk)
+   if request.method == 'POST':
+      form = ProductForm(request.POST, instance=item)
+      if form.is_valid():
+         form.save()
+         return redirect('dashboard-product')
+   else:
+      form = ProductForm( instance=item)
+
+   context = {
+      'form': form,
+   }
+   return render(request, 'dashboard/product_update.html', context)
+
+
 
 @login_required
 def order(request):
-   return render(request, 'dashboard/order.html')
+   return render(request, 'dashboard /order.html')
