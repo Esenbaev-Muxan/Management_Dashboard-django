3254752948be988e75a0a6dfa5d2d6de79fb0ecb
Student astrum <student@astrum.domain>
2023-02-17 10:51:06 +0500

    Update user profile

--- inventoryproject/urls.py
+++ inventoryproject/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', include('dashboard.urls')),
     path('register/', user_view.register, name='user-register'),
     path('profile/', user_view.profile, name='user-profile'),
+    path('profile/update/', user_view.profile_update, name='user-profile-update'),
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,5 @@
 from django import forms 
+from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -10,3 +11,13 @@ class CreateUserForm(UserCreationForm):
         model = User
         fields = ['username','email', 'password1', 'password2' ] 
 
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['address', 'phone', 'image']
