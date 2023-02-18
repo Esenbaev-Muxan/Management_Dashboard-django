c56df2079dde3781ba21c7cb3771dc83a02f443a
Esenbaev-Muxan <esenbaevmuxan@email.com>
2023-02-18 18:45:40 +0500

    setting admin dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,6 +10,7 @@ from django.contrib import messages
 @login_required
 def index(request):
    orders = Order.objects.all()
+   products = Product.objects.all()
    if request.method == 'POST':
       form = OrderForm(request.POST)
       if form.is_valid():
@@ -22,6 +23,7 @@ def index(request):
    context = {
       'orders': orders,
       'form': form,
+      'products': products,
    }
    return render(request, 'dashboard/index.html', context)
 
