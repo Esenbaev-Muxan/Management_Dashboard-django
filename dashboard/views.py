bc10e43d19601af262a2238da82bc79f9fcf10b4
Esenbaev-Muxan <esenbaevmuxan@email.com>
2023-02-18 14:22:49 +0500

    messages in added product

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Product, Order
 from .forms import ProductForm, OrderForm
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 @login_required
@@ -52,6 +53,8 @@ def product(request):
       form = ProductForm(request.POST)
       if form.is_valid():
          form.save()
+         product_name = form.cleaned_data.get('name')
+         messages.success(request, f'{product_name} has beed added')
          return redirect('dashboard-product')
    else:
       form = ProductForm()
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,6 +10,8 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account has been created for {username}. Continue to Log in')
             return redirect('user-login')
     else:
         form = CreateUserForm()  
